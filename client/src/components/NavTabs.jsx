f9d807a9ccfe8cda5fa921307bc3bf397e60aca5
fordefam12 <136336831+fordefam12@users.noreply.github.com>
2023-09-01 12:54:29 -0500

    file corrections

--- client/src/components/NavTabs.jsx
+++ client/src/components/NavTabs.jsx
@@ -1,54 +1,55 @@
+import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import '../App.scss'
+import LogoS from '../assets/images/fullAClogo.png'
 
-// Here we are using object destructuring assignment to pluck off our variables from the props object
-// We assign them to their own variable names
+
 function NavTabs() {
   const currentPage = useLocation().pathname;
 
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <Link
-          to="/Home"
-          // Check to see if the currentPage is `Home`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === "/" ? "nav-link active" : "nav-link"}
-        >
-          Home
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/Brands"
-          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={
-            currentPage === "/Brands" ? "nav-link active" : "nav-link"
-          }
-        >
-          Brands
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/Men"
-          // Check to see if the currentPage is `Men`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={
-            currentPage === "/Men" ? "nav-link active" : "nav-link"
-          }
-        >
-          Men
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/Women"
-          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={currentPage === "/Women" ? "nav-link active" : "nav-link"}
-        >
-          Women
-        </Link>
-      </li>
-    </ul>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/Home">
+        <img src={LogoS} alt="Logo" />
+      </Link>
+
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className={`nav-item ${currentPage === "/" ? "active" : ""}`}>
+            <Link className="nav-link" to="/Home">
+              Home
+            </Link>
+          </li>
+          <li className={`nav-item ${currentPage === "/Brands" ? "active" : ""}`}>
+            <Link className="nav-link" to="/Brands">
+              Brands
+            </Link>
+          </li>
+          <li className={`nav-item ${currentPage === "/Men" ? "active" : ""}`}>
+            <Link className="nav-link" to="/Men">
+              Men
+            </Link>
+          </li>
+          <li className={`nav-item ${currentPage === "/Women" ? "active" : ""}`}>
+            <Link className="nav-link" to="/Women">
+              Women
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
