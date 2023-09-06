2f6e7c54435f5ddf1e5c0024f2cefb5db16dbb84
safwaan41 <safwaan41@gmail.com>
2023-09-06 11:29:25 -0500

    login/logout functionality navtabs

--- client/src/components/NavTabs.jsx
+++ client/src/components/NavTabs.jsx
@@ -3,9 +3,13 @@ import { Link, useLocation } from "react-router-dom";
 import "../App.scss";
 import LogoS from "../assets/images/fullAClogo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import auth from "../utils/auth";
 
 function NavTabs() {
   const currentPage = useLocation().pathname;
+
+  
+
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -51,6 +55,29 @@ function NavTabs() {
               About
             </Link>
           </li>
+          {auth.loggedIn() ? (
+            // Render Logout link when the user is logged in
+            <li className={`nav-item ${currentPage === "/logout" ? "active" : ""}`}>
+              <Link className="nav-link" onClick= {()=>
+              auth.logout()}>
+                Logout
+              </Link>
+            </li>
+          ) : (
+            // Render Login and Signup links when the user is not logged in
+            <>
+              <li className={`nav-item ${currentPage === "/signup" ? "active" : ""}`}>
+                <Link className="nav-link" to="/signup">
+                  Signup
+                </Link>
+              </li>
+              <li className={`nav-item ${currentPage === "/login" ? "active" : ""}`}>
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
+              </li>
+            </>
+          )}
           <a href="/Cart" className="cart-link">
             <i className="fa fa-shopping-cart"></i>
             <span className="cart-counter">Cart</span>
